ecb4e32c24b46d480a3ad0729f10f39d4bc3b1f4
Hoang Hoi <hoanghoi1310@gmail.com>
2016-08-24 16:34:04 +0700

    add  model

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+    protected $table = 'comments';
+    protected $fillable = ['comment', 'post_id', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post', 'post_id');
+    }
+
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    protected $table = 'posts';
+    protected $fillable = ['title', 'body', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'post_id');
+    }
+
 }
