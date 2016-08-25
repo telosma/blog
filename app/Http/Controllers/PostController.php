65fddb33524cc05fa322f8f08a0166a509ae94cb
Hoang Hoi <hoanghoi1310@gmail.com>
2016-08-25 11:47:57 +0700

    them controller can thiet

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use Auth;
-
 use App\Http\Requests;
 
 class PostController extends Controller
 {
-    public function create(Request $request)
+
+    public function postCreate(Request $request)
     {
         $post = new Post;
         $post->title = $request->title;
@@ -19,4 +19,25 @@ class PostController extends Controller
         $post->save();
         return "them thanh cong";
     }
+
+    public function postDelete()
+    {
+        
+    }
+
+    public function postEdit()
+    {
+        
+    }
+
+    public function postAddComment()
+    {
+        
+    }
+
+    public function getListComment()
+    {
+        
+    }
+
 }
--- resources/views/includes/user/formPost.blade.php
+++ resources/views/includes/user/formPost.blade.php
@@ -1,9 +1,7 @@
 <div>
     <<form action="#" method="POST">
         <input type="text" name="title"/>
-        <textarea name="body">
-            
-        </textarea>
+        <textarea name="body"></textarea>
 
         <input type="submit" name="submit" value="submit"/>
 
